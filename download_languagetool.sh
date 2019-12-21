20802637597ead7d1fba887a8cba7d25fb18e49b
Pere Orga <pere@orga.cat>
2019-12-21 13:41:18 +0100

    Improve previous commit

--- download_languagetool.sh
+++ download_languagetool.sh
@@ -2,4 +2,4 @@
 
 curl https://raw.githubusercontent.com/Softcatala/catalan-dict-tools/master/resultats/lt/diccionari.txt > ./source/languagetool/diccionari.txt
 cut -d' ' -f1 ./source/languagetool/diccionari.txt > ./source/languagetool/paraules.txt
-egrep '^[A-Z]+' ./source/languagetool/paraules.txt > ./source/languagetool/majuscules.txt
+egrep '^[[:upper:]]+' ./source/languagetool/paraules.txt > ./source/languagetool/majuscules.txt
